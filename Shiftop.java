12e86d855605817ea449b21f0c03f4981e9cfd54
KOYEL GHOSH <112954938+itismeriya@users.noreply.github.com>
2023-04-17 13:29:37 +0530

    Add files via upload

---
+++ Shiftop.java
@@ -0,0 +1,11 @@
+class Shiftop{
+    public static void main(String[] args) {
+        int m=128;
+        m=m>>3;
+        System.out.println(m);
+        m=128;
+        m=m<<3;
+        System.out.println(m);
+        
+    }
+}

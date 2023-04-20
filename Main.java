829e7919f7e3c17e512e7d9b253752d07ecc31b1
KOYEL GHOSH <112954938+itismeriya@users.noreply.github.com>
2023-04-20 12:39:41 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+
+public class TriangleGame 
+{
+    
+public static void main(String[] args) 
+{
+  
+      
+        Scanner scanner = new Scanner(System.in);
+ 
+       System.out.println("Enter the angles");
+ 
+       int[] angles = new int[3];
+ 
+       for (int i = 0; i < 3; i++) 
+{
+
+           
+ angles[i] = scanner.nextInt();
+      
+  }
+       
+ scanner.close();
+
+   
+    
+        if (angles[0] + angles[1] + angles[2] != 180)
+ {
+            System.out.println("No Prize");
+        } 
+else if (angles[0] == angles[1] && angles[1] == angles[2]) {
+ 
+           System.out.println("Prize 3");
+      
+  } else if (angles[0] == 90 || angles[1] == 90 || angles[2] == 90) {
+     
+       if (angles[0] == angles[1] && angles[1] == angles[2]) {
+                
+System.out.println("Prize 3");
+            } 
+else {
+                System.out.println("Prize 2");
+            }
+      
+  } else {
+         
+   System.out.println("Prize 1");
+        }
+
+    }
+
+}
